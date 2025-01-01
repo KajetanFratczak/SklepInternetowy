43958f195742ef6cce2b470938f8061fd3464d19
Kajetan Frątczak <kfratczak@student.agh.edu.pl>
2025-01-01 18:17:10 +0100

    Logowanie dodane (wstępna wersja)

--- sklep_internetowy/src/components/Login.jsx
+++ sklep_internetowy/src/components/Login.jsx
@@ -1,9 +1,41 @@
-import React from 'react';
+import React, { useState } from 'react';
+import { useNavigate } from 'react-router-dom';
 
 const Login = () => {
+
+    const mockUsers = [
+        { username: "user1", password: "password1" },
+        { username: "user2", password: "password2" },
+        { username: "admin", password: "admin123" },
+      ];
+
+    const [username, setUsername] = useState("");
+    const [password, setPassword] = useState("");
+    const [error, setError] = useState("");
+    const navigate = useNavigate();
+
+    const handleLogin = () => {
+        const user = mockUsers.find((u) => u.username === username && u.password === password);
+        
+        if (user)
+        {
+            localStorage.setItem("loggedUser", JSON.stringify(user));
+            setError("");
+            navigate("/");
+        }
+        else
+        {
+            setError("Invalid username or password");
+        }
+    };
+
     return (
-        <div>
-            
+        <div className='login-page'>
+            <h1>Logowanie</h1>
+            <input type='text' placeholder='Username' value={username} onChange={(e) => setUsername(e.target.value)}></input>
+            <input type='password' placeholder='Password' value={password} onChange={(e) => setPassword(e.target.value)}></input>
+            <h2>{error}</h2>
+            <button onClick={handleLogin}>Login</button>
         </div>
     );
 };
